8b125884c4050c28141aea995fcd3ae60c875bc7
Theolorex <30479282+Theolorex@users.noreply.github.com>
2025-03-07 19:26:05 -0500

    rocco's current project

---
+++ Assets/Scripts/Rattattouille.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ratattouille : MonoBehaviour
+{
+
+    /*
+     
+    custom trait to be added to food types: 
+    
+    if(getobject(self) isTouching getobject(floor))
+    {
+    activate a custom timer
+    listen.(eventsthatmightinfluencetimer)
+    after timer ends: SummonRat().Invoke
+    }
+
+
+
+    rats listen in on the signal of spoiled food
+
+    if (spoiled food signal invoked)
+    {
+    //grab a random waiting rat (if there are no more waiting rats, spawn one in)
+    //activate RatActive state
+    }
+
+    Rat States
+    { Passive (waiting to be activated off screen, does nothing), Active (Main Default State), 
+
+
+    event ratpocalypse
+    (summon in a fuck ton of rats)
+
+    */
+
+
+  
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
