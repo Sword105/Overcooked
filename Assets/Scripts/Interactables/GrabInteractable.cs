f4533924c8e854203fde4691474beb32389ed360
TitanManager23 <jorgemarioalvarezmeza2005@gmail.com>
2025-03-07 21:18:48 -0500

    Removing Food Class

--- Assets/Scripts/Interactables/GrabInteractable.cs
+++ Assets/Scripts/Interactables/GrabInteractable.cs
@@ -11,6 +11,8 @@ public class GrabInteractable : Interactable
     //public AudioSource audioSource;
     public AudioClip interactSound;
     public ItemType itemType; 
+    public List<Recipe> recipeList;
+    public GameObject grabInteractable;
 
     //Determines whether a object is grabable based on whether it is being held or not
     void Update()
@@ -43,7 +45,21 @@ public class GrabInteractable : Interactable
                 AudioManager.instance.PlaySoundFX(interactSound, transform, 1f);
             }
         }
-        else
+        else if (transform.tag == "Grabbable"){
+
+            Collider[] nearbyObjects = Physics.OverlapSphere(player.transform.position + 
+            (player.transform.forward * player.GetComponent<PlayerInteraction>().interactionForwardOffset), player.GetComponent<PlayerInteraction>().interactionRange);
+
+             Collider nearestInteractable = player.GetComponent<PlayerInteraction>().FindClosestInteractable(nearbyObjects);
+
+             //Checking if we can combine two Food objects, and doing so
+            
+            if (FindRecipe(nearestInteractable.GetComponent<GrabInteractable>().itemType)!= null){
+                Debug.Log("Cooking available");
+                //GameObject result = Instantiate(FindRecipe(nearestInteractable.GetComponent<GrabInteractable>().itemType).outputFoodItem)
+            }
+
+        }
         {
             //This code is meant to swap the location between a held item and an item on the floor
             //Evidently, it was very buggy
@@ -54,5 +70,17 @@ public class GrabInteractable : Interactable
             heldItem.position = tempPosition;
             */
         }
+
+        Recipe FindRecipe(ItemType item)
+    {
+        foreach (Recipe recipe in recipeList)
+        {
+            if (recipe.inputFoodItem == item)
+            {
+                return recipe;
+            }
+        }
+        return null;
+    }
     }
 }
