54755c40fa3599f715250662a7c7f73904b96e04
David Bucio-Paz <daviddbp.buciopaz@gmail.com>
2025-03-24 22:27:24 -0400

    Dash
    Did dash :)

--- Assets/Movement Script 3.cs
+++ Assets/Movement Script 3.cs
@@ -1,5 +1,6 @@
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System.Collections;
 
 public class MovementScript3 : MonoBehaviour
 {
@@ -7,6 +8,23 @@ public class MovementScript3 : MonoBehaviour
     public Rigidbody player;
     public float speed = 10f;
     float weirdFloat;
+
+    
+    
+
+    [Header("Dashing")]
+    public bool dashing = true;
+
+    public int dashCounter = 5;
+
+    private int currentDashes;
+    private float dashingPower = 5f;
+    private float dashingTime = 0.1f;
+    private float dashingCooldown = 1f;
+    private bool isOnCoolDown = false;
+    private float coolDownDashing = 0.75f;
+  
+   
 
     private Vector3 movementInput = Vector3.zero;
 
@@ -16,8 +34,25 @@ public class MovementScript3 : MonoBehaviour
 
     }
 
+    void Start(){
+
+        currentDashes = dashCounter;
+
+    }
+
+
+    void Update() {
+
+       if(Input.GetKeyDown(KeyCode.Space) && dashing && !isOnCoolDown) {
+
+        StartCoroutine(Dash());
+
+       } 
+ }
+
    
     void FixedUpdate() {
+
 
         player.MovePosition((Vector3)transform.position + movementInput * speed * Time.deltaTime); // movementposition used for movement, vector3 for 3d
 
@@ -42,6 +77,29 @@ public class MovementScript3 : MonoBehaviour
 
 
 
+private IEnumerator Dash() {
+    dashing = true;
+    isOnCoolDown = true;
+    currentDashes--;
+
+
+    movementInput = new Vector3(transform.forward.x * dashingPower, 0f, transform.forward.z * dashingPower);
+    yield return new WaitForSeconds(dashingTime);
+    movementInput = Vector3.zero;
+
+    yield return new WaitForSeconds(dashingCooldown);
+    dashing = true;
+
+    yield return new WaitForSeconds(coolDownDashing);
+    isOnCoolDown = false;
+
+    if(currentDashes == 0){
+        dashing = false;
+        
+    }
+
+
+    
+ }
 
 }
-
