f0393cea6c9a11d316f66553365c771871e64760
Osvaldo Q. <quintanalosvaldo@gmail.com>
2025-04-01 20:05:40 -0400

    Beginning LevelManager
    Implemented a new LevelManager object, along with a timer and an Order list.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+[System.Serializable]
+public struct Order
+{
+    public GameObject NPC;
+    public List<ItemType> order;
+}
+
+public class LevelManager : MonoBehaviour
+{
+    public float maxTimeInSeconds = 300f;
+    private bool timeEnded = false;
+    private float currentTimeLeft;
+
+    public List<Order> orders;
+    public PlateStation plateStation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTimeLeft = maxTimeInSeconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeTick();
+    }
+
+    void TimeTick()
+    {
+        if (!timeEnded)
+        {            
+            currentTimeLeft -= Time.deltaTime;
+            Debug.Log(currentTimeLeft + " seconds left");
+
+            if (currentTimeLeft <= 0)
+            {
+                timeEnded = true;
+                Debug.Log("Clock ended");
+            }
+        }
+    }
+}
