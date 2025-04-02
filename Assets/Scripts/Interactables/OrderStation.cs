9ec2d4fb0ec6ac5ff6136a4925b9fca4c6ba13b2
Osvaldo Q. <quintanalosvaldo@gmail.com>
2025-04-01 21:30:32 -0400

    Order Submission
    The OrderStation and the plate returning system has been implemented. Later on, this will be more polished, but it is mostly functional.

---
+++ Assets/Scripts/Interactables/OrderStation.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderStation : StorageContainer
+{
+    public LevelManager levelManager;
+
+    public override void Interact(GameObject player, Transform heldItem)
+    {
+        if (heldItem.GetComponent<PlateInteractable>() != null)
+        {
+            foreach (Order x in levelManager.orders)
+            {
+                bool foundItem;
+                bool correctOrder = true;
+
+                if (heldItem.GetComponent<PlateInteractable>().foodStored.Count != x.order.Count)
+                {
+                    continue;
+                }
+
+                foreach (ItemType y in x.order)
+                {
+                    foundItem = false;
+                    for (int i = 0; i < heldItem.GetComponent<PlateInteractable>().foodStored.Count; i++)
+                    {
+                        if (y == heldItem.GetComponent<PlateInteractable>().foodStored[i])
+                        {
+                            foundItem = true;
+                        }
+                    }
+                    if (!foundItem)
+                    {
+                        correctOrder = false;
+                        break;
+                    }
+                }
+
+                if (correctOrder)
+                {
+                    Debug.Log("Found order");
+                    levelManager.orders.Remove(x);
+                    levelManager.plateStation.AddPlate();
+                    base.Interact(player, heldItem);
+                    Destroy(heldItem.GetComponent<Interactable>());
+                    break;
+                }
+            }
+        }
+    }
+}
