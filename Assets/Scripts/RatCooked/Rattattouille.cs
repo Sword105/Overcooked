7d93393012556ca6a41ce59617e362043d1a9de7
Theolorex <30479282+Theolorex@users.noreply.github.com>
2025-04-05 17:50:01 -0400

    addded stuff

--- Assets/Scripts/RatCooked/Rattattouille.cs
+++ Assets/Scripts/RatCooked/Rattattouille.cs
@@ -5,28 +5,13 @@ using UnityEngine;
 public class Ratattouille : MonoBehaviour
 {
 
-    /*
-     
-    custom trait to be added to food types: 
     
-    if(this.gameObject isTouching getobject(floor)) //note for self: look at the xr stuff and see how they do it
-    {
-    activate a custom timer giannisakritidis.com/blog/Unity-Timers/
-    (Timer should pause when food is picked up)
-    listen.(eventsthatmightinfluencetimer) POTENTIAL ISSUE: becuase the if statement depends on if its touching the floor
-    cant activate the timers deactivation
-    after timer ends: SummonRat().Invoke (use events cause it decouples)
-    }
-
-
-
-    rats listen in on the signal of spoiled food */
 
     //IDEA: if (spoiled food signal invoked)
     //grab a random waiting rat (if there are no more waiting rats, spawn one in, saves memory, uses preexisting ones first)
     
     
-    public static event summonRat //would go on the level or game manager
+    /*public static event summonRat //would go on the level or game manager
     {
         int ratCounter = 0; //must be persistently tracked throughout the level
         if (ratCounter < 11) //if the counter is less than the rats pre-placed in level 
@@ -38,7 +23,8 @@ public class Ratattouille : MonoBehaviour
             spawnRat(); //otherwise, create a new one (basically prefers cheaper method over spawning one in directly)
         }
             
-        findRat(int ratCounter) {
+        void findRat(int ratCounter) 
+        {
             if (rat[ratCounter] == Passive) 
             {
                 set ratstate for rat[ratCounter] as Active //POTENTIAL PROBLEM: removing a rat coutner and calling a rat after might bug out, might need a queue system
@@ -49,16 +35,15 @@ public class Ratattouille : MonoBehaviour
             }
         }
         
-        spawnRat() 
+        void spawnRat() 
         {
             public GameObject ratPrefab; //reference to the ratPrefab
         
             Instantiate(ratPrefab, findRatSpawn(), Quaternion.identity); //spawns in the ratPrefab after being given the map location at 0 rotation
         }
         
-        findRatSpawn() 
+        void findRatSpawn() 
         {
-
 
         }
     }
@@ -68,14 +53,14 @@ public class Ratattouille : MonoBehaviour
 
         public enum RatStates
         {
-        Passive /*(waiting to be activated off screen, does nothing)*/,
-        Active /*(Main Default Managing State)*/, 
-        Scurry /*(going to food)*/,
-        Eating /*(standing at food)*/, 
-        Grabbed /*(being held by a user, possibly also used when thrown)*/,
-        Pushing /*(annoying bastard pushing off stuff from counter)*/,
-        Escape /*(running away with food or not, sets itself as passive at the end)*/
-        }
+        Passive /*(waiting to be activated off screen, does nothing)*///,
+        //Active /*(Main Default Managing State)*///, 
+        //Scurry /*(going to food)*///,
+        //Eating /*(standing at food)*///, 
+        //Grabbed /*(being held by a user, possibly also used when thrown)*///,
+        //Pushing /*(annoying bastard pushing off stuff from counter)*///,
+        //Escape /*(running away with food or not, sets itself as passive at the end)*/
+        //}
 
 /*
         event ratpocalypse
---
+++ Assets/Scripts/RatCooked/SpoilTimer.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpoilTimer : MonoBehaviour
+{
+    /*
+
+    custom script trait to be added to food prefabs:
+
+    if(this.gameObject isTouching getobject(floor)) //note for self: look at the xr stuff and see how they do it
+    {
+    activate a custom timer giannisakritidis.com/blog/Unity-Timers/
+    (Timer should pause when food is picked up)
+    listen.(eventsthatmightinfluencetimer) POTENTIAL ISSUE: becuase the if statement depends on if its touching the floor
+    cant activate the timers deactivation
+    after timer ends: SummonRat().Invoke (use events cause it decouples)
+    }
+
+
+
+    rats listen in on the signal of spoiled food */
+
+    [Header("Food is Touching Ground")]
+    [Tooltip("If the food is touching the ground")]
+    public bool OnGround = false;
+    [Tooltip("Kinda only matters if the ground is ever not flat but here just in case")]
+    public float GroundedOffset = -0.14f;
+    [Tooltip("sphere radius for the grounded check")]
+    public float GroundedRadius = 0.5f;
+    [Tooltip("the layer counted as ground")]
+    public LayerMask GroundLayers;
+    
+    private void MainProcess()
+    {
+       if (OnGround)
+       {
+           //activate timer
+       } 
+    }
+
+    private void Update()
+    {
+        GroundedCheck();
+    }
+
+    private void GroundedCheck()
+    {
+        //first creates a spehere at the transforms position (basically at the foods position)
+        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z);
+        //then checks if the sphere is touching a grounded layer using the position and radius, sets OnGround to true if touching a grounded layer
+        OnGround = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z), GroundedRadius);
+    }
+    
+    
+}
