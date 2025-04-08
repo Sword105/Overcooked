2ce29d9fdf00392957cb7dab1f3e77beef40d601
David Bucio-Paz <daviddbp.buciopaz@gmail.com>
2025-04-07 21:02:30 -0400

    Dash update New Assets
    Dash updated with Osvaldo coding and me supporting
    
    New assets for the starbucks level

--- Assets/Movement Script 3.cs
+++ Assets/Movement Script 3.cs
@@ -13,7 +13,7 @@ public class MovementScript3 : MonoBehaviour
     
 
     [Header("Dashing")]
-    public bool dashing = true;
+    public bool dashing = false;
 
     public int dashCounter = 5;
 
@@ -26,21 +26,24 @@ public class MovementScript3 : MonoBehaviour
 
    
 
-    private Vector3 movementInput = Vector3.zero;
-   // private bool dashed = false;
+    private Vector3 movementInput;
+    private bool dashed = false;
     
 
 
     public void OnMove(InputAction.CallbackContext context) {
-        movementInput = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
-
+        if (!dashing)
+        {
+            movementInput = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
+        }
     }
 
-   /* public void OnDash(InputAction.CallbackContext context){
-        dashed = context.ReadValue<bool>();
-        dashed = context.action.triggered;
+    public void OnDash(InputAction.CallbackContext context){
+        if(context.performed){
+            dashed = true;
+        }
     }
-   */
+   
 
     void Start(){
 
@@ -51,18 +54,25 @@ public class MovementScript3 : MonoBehaviour
 
     void Update() {
 
-       if(Input.GetKeyDown(KeyCode.Space) && dashing && !isOnCoolDown) {
+       if(dashed && !isOnCoolDown) {
 
         StartCoroutine(Dash());
+        
 
-       } 
+       }
+
+       dashed = false; 
+
  }
 
    
     void FixedUpdate() {
 
-
-        player.MovePosition((Vector3)transform.position + movementInput * speed * Time.deltaTime); // movementposition used for movement, vector3 for 3d
+        if (!Physics.Raycast(transform.position, transform.forward, 0.8f))
+        {
+            player.MovePosition((Vector3)transform.position + movementInput * speed * Time.deltaTime); // movementposition used for movement, vector3 for 3d
+        }
+        
 
         //makes the magnitude thing so that if you are clicking on a button or not - keeps character looking in the same direction
         if(movementInput.magnitude >=0.1f){
@@ -74,40 +84,23 @@ public class MovementScript3 : MonoBehaviour
                                                                                                     //has a weird float variable that is needed - not known why
 
         transform.rotation = Quaternion.Euler(0, smooth, 0); // used the smooth variable that was retrieved to make the rotation of character face the direction smoothly
-
-        
-
         }
-
-    
-
     }
 
+    private IEnumerator Dash() {
+        dashing = true;
+        isOnCoolDown = true;
+        //currentDashes--;
 
 
-   private IEnumerator Dash() {
-    dashing = true;
-    isOnCoolDown = true;
-    currentDashes--;
-
-
-    movementInput = new Vector3(transform.forward.x * dashingPower, 0f, transform.forward.z * dashingPower);
-    yield return new WaitForSeconds(dashingTime);
-    movementInput = Vector3.zero;
-
-    yield return new WaitForSeconds(dashingCooldown);
-    dashing = true;
-
-    yield return new WaitForSeconds(coolDownDashing);
-    isOnCoolDown = false;
-
-    if(currentDashes == 0){
+        movementInput = new Vector3(transform.forward.x * dashingPower, 0f, transform.forward.z * dashingPower);
+        yield return new WaitForSeconds(dashingTime);
+        movementInput = Vector3.zero;
         dashing = false;
+
+        yield return new WaitForSeconds(coolDownDashing);
+        isOnCoolDown = false;
         
     }
-
-
-    
- }
 
 }
