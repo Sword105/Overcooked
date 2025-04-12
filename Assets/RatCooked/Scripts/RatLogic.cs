e84a2a5cf201817d9b79574301601e71adcd1ca4
Theolorex <30479282+Theolorex@users.noreply.github.com>
2025-04-12 04:18:20 -0400

    more done more done

--- Assets/RatCooked/Scripts/RatLogic.cs
+++ Assets/RatCooked/Scripts/RatLogic.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,11 +20,20 @@ public class RatLogic : MonoBehaviour
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform target;
     [SerializeField] RatState state = RatState.ACTIVE;
+    [SerializeField] float distanceThreshold = 0.5f;
+    [SerializeField] bool isMoving = false;
+    [SerializeField] float eatingTimerMax = 4f;
+     private float currentEatingTimer = 4f;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         state = RatState.ACTIVE; //sets default state
+    }
+
+    private void Start()
+    {
+        currentEatingTimer = eatingTimerMax;
     }
 
     public void SetTarget(Transform newTarget) //how to change destination
@@ -32,7 +42,6 @@ public class RatLogic : MonoBehaviour
         if (agent != null && target != null)
         {
             agent.SetDestination(target.position);
-            RatState state = RatState.SCURRY;
             Debug.Log("Rat heading to " + target.position);
             Debug.Log(state);
         }
@@ -41,6 +50,50 @@ public class RatLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*if (grabbed)
+        {
+            state = RatState.GRABBED;
+        }*/
         
+        switch (state)
+        {
+            case RatState.ACTIVE:
+                if (isMoving)
+                {
+                    RatState state = RatState.SCURRY;
+                }
+                break;
+            case RatState.SCURRY:
+                Debug.Log(transform.position + " " + target.position);
+                if (Vector3.Distance(transform.position, target.position) <= distanceThreshold) //when reaching target...
+                {
+                    Debug.Log("Rat at " + target.position + ", switching to eating");
+                    SetTarget(transform);
+                    state = RatState.EATING;
+                }
+                break;
+            case RatState.EATING:
+                if (OnGround) //personal item timer only ticks down when still on the ground, preserves the timer and pauses it otherwise
+                {
+                    currentEatingTimer -= Time.deltaTime; //timer ticking down
+                }
+        
+                if (currentEatingTimer <= 0) //end condition, duhh
+                {
+                    TimerEnded = true;
+                    currentEatingTimer = 0;
+                    //delete food prefab
+                    state = RatState.ESCAPING;
+                }
+                break;
+            case RatState.GRABBED:
+                break;
+            case RatState.PUSHING:
+                break;
+            case RatState.ESCAPING:
+                //sets target lcoation to the spawn points used as escape points, follows same system
+                //If reached escape point, invoke rat escaped event (prolly used for ratpocalypse and pushing)
+                break;
+        }
     }
 }
