d8f520ac873157d9ceb7590b4acee61dceb650b3
Osvaldo-Q <quintanalosvaldo@gmail.com>
2025-04-17 11:15:48 -0400

    Model Updating Working
    There are issues with the models themselves, but the models do in fact load now per player.

--- Assets/Scripts/UI Scripts/PlayerJoining/SpawnPlayers.cs
+++ Assets/Scripts/UI Scripts/PlayerJoining/SpawnPlayers.cs
@@ -13,6 +13,7 @@ public class SpawnPlayers : MonoBehaviour
         for (int i = 0; i < playerConfigs.Length; i++)
         {
             GameObject player = Instantiate(playerPrefab, playerSpawns[i].position + new Vector3(0,5,0), playerSpawns[i].rotation, gameObject.transform);
+            player.GetComponent<MeshFilter>().mesh = playerConfigs[i].playerModel;
             player.GetComponent<PlayerInputHandler>().StartPlayer(playerConfigs[i]);
         }
     }
